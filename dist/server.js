1c7f4ca1e6dfe0534d056254254e1fbe14d1cc8d
chandra655 <chandra.penugonda@unacademy.com>
2020-12-26 22:12:57 +0530

    classes

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var person = {
-    name: 'chandra',
-    age: 23,
-    gender: 'male'
-};
+var Human = /** @class */ (function () {
+    function Human(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    return Human;
+}());
+var person = new Human('chandra', 23, 'male');
 var sayHi = function (_a) {
     var name = _a.name, age = _a.age, gender = _a.gender;
     return "Hi " + name + ", age " + age + " and gender " + gender + "!";
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string | undefined;
+  constructor(name: string, age: number, gender?: string | undefined) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: 'chandra',
-  age: 23,
-  gender: 'male'
-};
+const person = new Human('chandra', 23, 'male');
 
 const sayHi = ({ name, age, gender }: Human): string => {
   return `Hi ${name}, age ${age} and gender ${gender}!`;
