eafe534ec1ff1afed1b65245fa0aa0fe18e453a2
chandra655 <chandra.penugonda@unacademy.com>
2020-12-27 07:40:18 +0530

    add block

--- dist/server.js
+++ dist/server.js
@@ -15,6 +15,13 @@ var Block = /** @class */ (function () {
     Block.createHash = function (index, prevHash, data, timestamp) {
         return sha256_1.default(index + prevHash + data + timestamp).toString();
     };
+    Block.validHash = function (currentBlock) {
+        return (typeof currentBlock.index === 'number' &&
+            typeof currentBlock.hash === 'string' &&
+            typeof currentBlock.prevHash === 'string' &&
+            typeof currentBlock.data === 'string' &&
+            typeof currentBlock.timestamp === 'number');
+    };
     return Block;
 }());
 var genesisBlock = new Block(0, '123', '', 'hello', 12345);
@@ -22,5 +29,40 @@ var blockChain = [genesisBlock];
 var getBlockChain = function () { return blockChain; };
 var getLatestBlock = function () { return blockChain[blockChain.length - 1]; };
 var getNewTimeStamp = function () { return Math.round(new Date().getTime() / 1000); };
-console.log(getBlockChain(), getLatestBlock(), getNewTimeStamp());
+var createBlock = function (data) {
+    var lastestBlock = getLatestBlock();
+    var index = lastestBlock.index + 1;
+    var prevHash = lastestBlock.hash;
+    var timestamp = getNewTimeStamp();
+    var hash = Block.createHash(index, prevHash, data, timestamp);
+    var newBlock = new Block(index, hash, prevHash, data, timestamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+var validBlock = function (currentBlock, prevBlock) {
+    var index = currentBlock.index, hash = currentBlock.hash, prevHash = currentBlock.prevHash, data = currentBlock.data, timestamp = currentBlock.timestamp;
+    if (currentBlock.index !== prevBlock.index + 1) {
+        return false;
+    }
+    else if (currentBlock.prevHash !== prevBlock.hash) {
+        return false;
+    }
+    else if (!Block.validHash(currentBlock)) {
+        return false;
+    }
+    else if (Block.createHash(index, prevHash, data, timestamp) !== hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+var addBlock = function (block) {
+    if (validBlock(block, blockChain[blockChain.length - 1])) {
+        blockChain.push(block);
+    }
+};
+createBlock('chandra');
+createBlock('reddy');
+console.log(getBlockChain());
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,16 @@ class Block {
   ) {
     return sha256(index + prevHash + data + timestamp).toString();
   }
+
+  static validHash(currentBlock: Block) {
+    return (
+      typeof currentBlock.index === 'number' &&
+      typeof currentBlock.hash === 'string' &&
+      typeof currentBlock.prevHash === 'string' &&
+      typeof currentBlock.data === 'string' &&
+      typeof currentBlock.timestamp === 'number'
+    );
+  }
 }
 const genesisBlock: Block = new Block(0, '123', '', 'hello', 12345);
 
@@ -38,6 +48,39 @@ const getBlockChain = (): Block[] => blockChain;
 const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-console.log(getBlockChain(), getLatestBlock(), getNewTimeStamp());
+const createBlock = (data: string): Block => {
+  const lastestBlock = getLatestBlock();
+  const index = lastestBlock.index + 1;
+  const prevHash = lastestBlock.hash;
+  const timestamp = getNewTimeStamp();
+  const hash = Block.createHash(index, prevHash, data, timestamp);
+  const newBlock = new Block(index, hash, prevHash, data, timestamp);
+  addBlock(newBlock);
+  return newBlock;
+};
 
+const validBlock = (currentBlock: Block, prevBlock: Block): boolean => {
+  const { index, hash, prevHash, data, timestamp } = currentBlock;
+  if (currentBlock.index !== prevBlock.index + 1) {
+    return false;
+  } else if (currentBlock.prevHash !== prevBlock.hash) {
+    return false;
+  } else if (!Block.validHash(currentBlock)) {
+    return false;
+  } else if (Block.createHash(index, prevHash, data, timestamp) !== hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (block: Block): void => {
+  if (validBlock(block, blockChain[blockChain.length - 1])) {
+    blockChain.push(block);
+  }
+};
+
+createBlock('chandra');
+createBlock('reddy');
+console.log(getBlockChain());
 export {};
